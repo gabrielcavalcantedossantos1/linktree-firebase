bea2d9c5d73575d33b57e3381c2fdded86a5d5bb
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-16 22:39:55 -0300

    enviando os dados iniciais

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import NetWorks from './pages/netWorks/NetWorks';
+import Login from './pages/login/Login';
+import Admin from './pages/admin/Admin';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/netWorks',
+    element: <NetWorks />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/admin',
+    element: <Admin />,
+  },
+]);
+
+export { router };
---
+++ src/pages/netWorks/NetWorks.tsx
@@ -0,0 +1,10 @@
+
+const NetWorks = () => {
+  return (
+    <div>
+      <h1>pagina NetWorks</h1>
+    </div>
+  );
+};
+
+export default NetWorks;
