1055880c71e0edd7762ab3cb0545b5ca43bea449
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-18 13:08:57 -0300

    criando a aba (MEUS LINKS)

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Header } from '../../components/header';
-import { Input } from '../../components/input';
 import { FormField } from '../../components/formField';
+
+import { FiTrash } from 'react-icons/fi';
 
 const Admin = () => {
   const [nameInput, setNameInput] = useState('');
@@ -64,24 +65,47 @@ const Admin = () => {
           </div>
         </section>
 
-        <div className="flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
-          <label className="text-white font-medium mt-2 mb-3">
-            Veja como está ficando
-          </label>
-          <article
-            className="w-11/12 max-w-xl flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
-            style={{
-              marginBottom: 8,
-              marginTop: 8,
-              backgroundColor: bgColorInput,
-            }}
-          >
-            <p className='font-medium'
-              style={{color:textColorInput}}
-            >{nameInput ? nameInput : 'Veja seu link aqui'}</p>
-          </article>
-        </div>
+        {nameInput && (
+          <div className="flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+            <label className="text-white font-medium mt-2 mb-3">
+              Veja como está ficando
+            </label>
+            <article
+              className="w-11/12 max-w-xl flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
+              style={{
+                marginBottom: 8,
+                marginTop: 8,
+                backgroundColor: bgColorInput,
+              }}
+            >
+              <p className="font-medium" style={{ color: textColorInput }}>
+                {nameInput}
+              </p>
+            </article>
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center"
+        >
+          Cadastrar
+        </button>
       </form>
+
+      <h2 className="text-white text-white mb-4 text-2xl">Meus links</h2>
+
+      <article
+        className="flex items-center justify-between w-11/12 max-x-xl rounded py-3 px-2 mb-2 select-none"
+        style={{ backgroundColor: '#fff', color: '#000' }}
+      >
+        <p>Canal do youtube</p>
+        <div>
+          <button className="border border-dashed p-1 bg-neutral-950 cursor-pointer opacity-90 hover:opacity-100  transition-opacity rounded-md">
+            <FiTrash size={18} color="#fff" />
+          </button>
+        </div>
+      </article>
     </div>
   );
 };
